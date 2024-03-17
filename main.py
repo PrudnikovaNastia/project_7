46964d517399de2e2c1c6c4a6ae8ba06588f29be
PrudnikovaNastia <nastia.prud.03@gmail.com>
2024-03-17 12:00:13 +0200

    realisation_function

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from app.io.input import input_text
+from app.io.input import input_text, read_file_builtin, read_file_with_pandas
 from app.io.output import output_text, write_to_file
 
 def main():
-    pass
+    text = input_text()
 
+    output_text(text)
+
+    write_to_file("output.txt", text)
+
+    print("Function for reading from a file using Python's built-in capabilities"+read_file_builtin("output.txt")+" ")
+    print(read_file_with_pandas("output.txt"))
 
 if __name__ == '__main__':
     main()
